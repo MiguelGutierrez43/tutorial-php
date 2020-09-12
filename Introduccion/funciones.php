4222610886614a661fb99cf8dccc529d4b185fe4
Miguel Gutierrez <miguelangel@MacBook-Air-de-Miguel.local>
2020-09-11 23:47:05 -0300

    Orden de carpetas

---
+++ Introduccion/funciones.php
@@ -0,0 +1,26 @@
+<?php
+
+function bienvenida(){
+
+    echo "Bienvenidos al curso de PHP";
+
+}
+
+bienvenida();
+
+echo '<br><br>';
+
+//Funcion con parametro
+
+function saludar($hola){
+
+    echo $hola;
+}
+
+saludar("Hola a todos");
+
+
+
+
+
+?>
